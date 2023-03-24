f87809d9f3ae7a6e9ce7c18c2e2ad46b756191d0
pro5un <nonameontop@naver.com>
2023-03-24 03:21:22 +0000

    230324

--- js/krish.js
+++ js/krish.js
@@ -3,13 +3,23 @@ const text = document.querySelector('h2');
 window.addEventListener('scroll', () => {
     const current = scrollY;
     text.style.fontSize = `clamp(3rem, ${current}px, 30rem)`;
-})
+});
+
+
+var cursor = document.getElementById('cursor');
+cursor.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+});
 
 
 
-const banner = document.querySelector(".container");
 
-window.addEventListener('scroll', function(){
-    const value = 1400 - window.scrollY;
-    banner.style.clipPath = "circle("+ value +"px at center center)";
-})
+// const banner = document.querySelector(".container");
+
+// window.addEventListener('scroll', function(){
+//     const value = 1100 - window.scrollY;
+//     banner.style.clipPath = "circle("+ value +"px at center center)";
+// })
